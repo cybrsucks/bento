09b8c7f5c878474e0d00cc50c70a1fd9085f3466
cybrsucks <cybrs36@gmail.com>
2023-05-17 04:50:53 +1000

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- js/script.js
+++ js/script.js
@@ -23,21 +23,13 @@ document.getElementById("daydate").innerHTML = day + ", " + month + " " + day_of
 
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
+const backdrop = document.querySelector('.backdrop');
 
 menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
+    backdrop.classList.toggle('is-active');
 });
-
-
-
-
-
-
-
-
-
-
 
 
 
