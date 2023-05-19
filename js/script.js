321a927b8fa2dacad79ecf040de5dca2b918f3f0
cybrsucks <cybrs36@gmail.com>
2023-05-19 22:59:37 +1000

    stable ver

--- js/script.js
+++ js/script.js
@@ -35,6 +35,44 @@ menu_btn.addEventListener('click', function () {
 
 
 
+var modal = document.getElementById("recipeModal");
+var editRecipeBtn = document.getElementsByClassName("editRecipeBtn");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+for (var i=0; i< editRecipeBtn.length; i++) {
+    editRecipeBtn[i].addEventListener("click", (e) => {
+        modal.style.display = "block";
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // MODE SWITCH
 
 const toggleBtn = document.getElementById("toggle-btn");
@@ -409,39 +447,6 @@ accordions.forEach(accordion => {
 
 
 
-
-
-
-
-
-
-// Get the modal
-var modal = document.getElementById("recipeModal");
-
-// Get the button that opens the modal
-var editRecipeBtn = document.getElementsByClassName("editRecipeBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-
-for (var i=0; i< editRecipeBtn.length; i++) {
-    editRecipeBtn[i].addEventListener("click", (e) => {
-        modal.style.display = "block";
-    });
-}
 
 
 
