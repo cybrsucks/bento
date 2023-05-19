e4a2259899872714d4b5f057ef051cc76b290dbc
cybrsucks <cybrs36@gmail.com>
2023-05-19 21:33:50 +1000

    manageRecipes and edit recipe modal

--- js/script.js
+++ js/script.js
@@ -411,3 +411,42 @@ accordions.forEach(accordion => {
 
 
 
+
+
+
+
+// Get the modal
+var modal = document.getElementById("recipeModal");
+
+// Get the button that opens the modal
+var editRecipeBtn = document.getElementsByClassName("editRecipeBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+for (var i=0; i< editRecipeBtn.length; i++) {
+    editRecipeBtn[i].addEventListener("click", (e) => {
+        modal.style.display = "block";
+    });
+}
+
+
+
+
+
+
+
+
